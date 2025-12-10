f3b760083d4ffcb53c31c124dcec0cbeebdb453f
dej-xx01 <101282484+dej-xx01@users.noreply.github.com>
2025-12-10 19:32:45 +0100

    made some style changes to this component

--- my-app/src/components/mainlayout.jsx
+++ my-app/src/components/mainlayout.jsx
@@ -1,39 +1,48 @@
 import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import { Link } from "react-router-dom"
+import ham from "../assets/images/ham.png"
+import close from "../assets/images/close.png"
+import tiktok from "../assets/images/tiktok1.png"
+import IG from "../assets/images/IG.png"
 const MainLayout = () =>{
       const [menuClicked, setMenuClicked] = useState(false)
       const navBtn = () =>{
             setMenuClicked(!menuClicked)
       }
       return(
-            <div className="bg-black h-screen">
-                  <nav className=" fixed inset-x-0">
-                        <div className=" flex  border-2 justify-between items-center relative">
-                        <Link to = "/">
-                              <div className= "flex flex-col w-fit">
-                                    <h1 className="text-white italic">Ajike</h1>
-                                    <h3 className="text-gray-400  italic">Model</h3>
+            <div className="bg-black min-h-screen relative">
+                  <nav className=" fixed inset-x-0 top-0">
+                        <div className=" flex border-2 justify-between items-center relative py-4">
+                              <Link to = "/">
+                                    <div className= "flex flex-col w-fit  leading-5 pl-2">
+                                          <h1 className="text-white font-serif text-[23px] font-medium ">Àjíké</h1>
+                                          <h3 className="text-gray-400  italic text-[13px]">Model</h3>
+                                    </div>
+                              </Link>
+                              <div className = {menuClicked ? "order-2 border h-screen absolute bg-white -inset-1.5" : "hidden" }>
+                                    <ul className = "flex flex-col h-full justify-center items-center border border-amber-900 gap-y-4 " >
+                                          <Link to = "/"><li onClick={navBtn}>Portfolio</li></Link>
+                                          <Link to= "/about"><li onClick={navBtn}>About</li></Link>
+                                          <Link to = "/links"><li onClick={navBtn}>My Links</li></Link>
+                                          <Link to = "/contact"><li onClick={navBtn}>Contact</li></Link>
+                                          {menuClicked && (
+                                                <div className="flex gap-x-3 mt-6">
+                                                      <a href="google.com"><img src= {tiktok} className="w-5"/></a>
+                                                      <a href="google.com"><img src= {IG} className="w-5"/></a>                                                      
+                                                </div>
+
+                                          )}
+                                    </ul>
                               </div>
-                        </Link>
-                        <div className = {menuClicked ? "order-2 border absolute inset-0 h-screen bg-white" : "hidden"}>
-                              <ul className = "flex flex-col">
-                                    <Link to = "/"><li onClick={navBtn}>Portfolio</li></Link>
-                                    <Link to= "/about"><li onClick={navBtn}>About</li></Link>
-                                    <Link to = "/links"><li onClick={navBtn}>My Links</li></Link>
-                                    <Link to = "/contact"><li onClick={navBtn}>Contact</li></Link>
-                                    <Link><li></li></Link>
-                              </ul>
-                        </div>
-                        <button className="text-black bg-white w-5 order-1 z-10" onClick={navBtn}>X</button>
+                              <img src={menuClicked? close : ham} className=" w-6 order-1 z-10 pr-2" onClick={navBtn}/>
                         </div>
 
                   </nav>
                   <Outlet/>
-                  {!menuClicked && <footer className="bg-white h-10 w-full fixed bottom-0">
-                        <div className="text-black text-center pt-5">
-                              <h1>This is deadass the footer</h1>
-                        </div>
+                  {!menuClicked && <footer className=" py-3 pl-2 w-full fixed bottom-0 ">
+                              <p className= "text-gray-600 text-[10px] font-serif">&copy; 2025 Àjíké. All rights reserved.</p>
+                              <p className= "text-gray-800 text-[11px] font-sans">Built by JED</p>
                   </footer>} 
             </div>
       )
