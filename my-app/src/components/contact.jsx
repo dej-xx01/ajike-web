b281d8f22e33d0310227bbbb7b511bf514f2ba82
dej-xx01 <101282484+dej-xx01@users.noreply.github.com>
2025-12-14 17:06:11 +0100

    created the form for the contact page

--- my-app/src/components/contact.jsx
+++ my-app/src/components/contact.jsx
@@ -1,6 +1,29 @@
 const Contact = () =>{
     return(
-        <h1>Contact us</h1>
+        <section className="pt-25 px-3">
+            <form action="">
+                <label htmlFor="name" className="text-white">
+                    Your name
+                    <input type="text" className="form-input" id="name" placeholder="Enter your name"/>
+                </label>
+                <label htmlFor="email" className="text-white">
+                    Your Email
+                    <input type="text" className="form-input" id="email" placeholder="you@example.com"/>
+                </label>
+                <label htmlFor="timeframe" className="text-white">
+                    Timeframe (optional)
+                    <input type="text" className="form-input" id="timeframe" placeholder="Enter date or approx timeframe"/>
+                </label>
+                <label htmlFor="budget" className="text-white">
+                    Project Budget (optional)
+                    <input type="text" className="form-input" id="budget" placeholder="what is your budget?"/>
+                </label>
+                <label htmlFor="message" className="text-white">
+                    Message
+                    <input type="text" className="form-input h-30" id="message" placeholder="Enter your message"/>
+                </label>                                                                
+            </form>
+        </section>
     )
 }
 
